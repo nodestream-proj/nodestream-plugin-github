256b71da1acbaba24561a923161a20ed1ba7ad20
Jon Bristow <jonathan_bristow@intuit.com>
2025-01-08 13:20:31 -0800

    chore: add github REST doc links to teams

--- nodestream_github/teams.py
+++ nodestream_github/teams.py
@@ -1,6 +1,6 @@
 from typing import AsyncIterator
 
-from httpx import HTTPError, HTTPStatusError
+import httpx
 from nodestream.pipeline import Extractor
 
 from .interpretations.relationship.repository import simplify_repo
@@ -12,7 +12,7 @@ from .types import (
     SimplifiedUser,
     TeamRecord,
 )
-from .util import GithubRestApiClient, init_logger
+from .util import GithubRestApiClient, PermissionCategory, PermissionName, init_logger
 
 logger = init_logger(__name__)
 
@@ -30,12 +30,23 @@ class GithubTeamsExtractor(Extractor):
     async def _fetch_members_by_role(
         self, team: GithubTeam, role: str
     ) -> AsyncIterator[SimplifiedUser]:
+        """Fetch all users that have a given role for a specified team.
+
+        These endpoints are only available to authenticated members of the
+        team's organization.
+
+        Access tokens require the read:org scope.
+
+        To list members in a team, the team must be visible to the authenticated user.
+
+        https://docs.github.com/en/enterprise-server@3.12/rest/teams/members?apiVersion=2022-11-28#list-team-members-legacy
+        """
         try:
             async for member in self.client.get(
                 f"teams/{team["id"]}/members", {"role": role}
             ):
                 yield simplify_user(member) | {"role": role}
-        except HTTPStatusError:
+        except httpx.HTTPError:
             logger.warning(
                 "Problem getting %s members for team '%s/%s'",
                 role,
@@ -57,26 +68,52 @@ class GithubTeamsExtractor(Extractor):
             yield member
 
     async def _fetch_teams(self, login):
+        """Fetch all teams in an organization that are visible to the authenticated user.
+
+        https://docs.github.com/en/enterprise-server@3.12/rest/teams/teams?apiVersion=2022-11-28#list-teams
+
+        Fine-grained tokens must have the "Members" organization permissions (read)
+        """
         try:
             logger.debug("Getting teams for %s", login)
             async for team_summary in self.client.get(
                 f"orgs/{login}/teams",
             ):
                 yield await self._fetch_team(login, team_summary)
-        except HTTPStatusError as e:
-            logger.debug("Problem getting team info for org '%s': %s", login, e)
-        except HTTPError as e:
-            logger.warning("Major problem getting team info for org '%s': %s", login, e)
+        except httpx.HTTPStatusError as e:
+            if e.response.status_code == httpx.codes.FORBIDDEN:
+                logger.permission_warning(
+                    "org teams",
+                    login,
+                    PermissionName.MEMBERS,
+                    PermissionCategory.ORG,
+                    exc_info=True,
+                )
+            else:
+                logger.warning(
+                    "Problem getting team info for org '%s'", login, exc_info=True
+                )
+        except httpx.HTTPError:
+            logger.warning(
+                "Major problem getting team info for org '%s'", login, exc_info=True
+            )
 
     async def _fetch_repos(self, team: GithubTeam) -> AsyncIterator[SimplifiedRepo]:
+        """Fetch all repos for a specified team that are visible to the authenticated user.
+
+        These endpoints are only available to authenticated members of the
+        team's organization.
+
+        https://docs.github.com/en/enterprise-server@3.12/rest/teams/teams?apiVersion=2022-11-28#list-team-repositories
+        """
         try:
             async for repo in self.client.get(f"teams/{team["id"]}/repos"):
                 yield simplify_repo(repo)
-        except HTTPStatusError:
+        except httpx.HTTPError:
             logger.warning(
-                "Problem getting repos for team '%s/%s': %s",
+                "Problem getting repos for team '%s/%s'",
                 team["organization"]["login"],
-                team["slug"],
+                team["id"],
                 exc_info=True,
             )
 
